cf5b741c2742046fe5c9df1329651c006b6b2073
vinay Kumar ranjan <vinay.raj.apple@gmail.com>
2024-04-30 08:30:58 +0530

    Test

---
+++ FruitDemo/FruitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitDemoApp.swift
+//  FruitDemo
+//
+//  Created by vinay Kumar ranjan on 30/04/24.
+//
+
+import SwiftUI
+
+@main
+struct FruitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
